6425ca703cdbff9a9d81b79677d22360d366acdc
Alvin Qi <peng.edy@gmail.com>
2017-01-19 02:42:37 +0800

    add model

--- main.go
+++ main.go
@@ -3,7 +3,16 @@ package main
 import (
 	_ "sitepointgoapp/routers"
 	"github.com/astaxie/beego"
+  "github.com/astaxie/beego/orm"
+  _ "github.com/mattn/go-sqlite3"
+  models "sitepointgoapp/models"
 )
+
+func init() {
+  orm.RegisterDriver("sqlite", orm.DR_Sqlite)
+  orm.RegisterDataBase("default", "sqlite3", "database/orm_test.db")
+  orm.RegisterModel(new(models.Article))
+}
 
 func main() {
 	beego.Run()
